feee7813189fef04f991918ea15024fefe210257
Max Barnash <i.am@anhero.ru>
2015-01-12 03:42:16 +0700

    0.8.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arre/protobuf "0.8.2-SNAPSHOT"
+(defproject arre/protobuf "0.8.2"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
